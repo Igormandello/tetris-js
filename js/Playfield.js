e603f8be6dc941255056f3c4fa1fb429e209a57a
Igor Mandello <igormandello@gmail.com>
2019-01-01 21:21:56 -0200

    Implements a simple piece render based on a random matrix

--- js/Playfield.js
+++ js/Playfield.js
@@ -13,6 +13,20 @@ class Playfield {
 
     this._w = this._cols * (this._cellSize + 2);
     this._h = this._rows * (this._cellSize + 2);
+
+    this._field = [];
+    for (let y = 0; y < this._rows; y++) {
+      let row = [];
+      for (let x = 0; x < this._cols; x++)
+        if (Math.random() * y > 5)
+          row.push(Math.floor(Math.random() * 3));
+        else
+          row.push(-1);
+
+      this._field.push(row);
+    }
+
+    console.log(this._field);
   }
 
   get update() {
@@ -26,10 +40,28 @@ class Playfield {
     ctx.fillStyle = '#000';
     ctx.fillRect(10, 10, this._w, this._h);
 
-    ctx.fillStyle = '#FFF';
     for (let y = 0; y < this._rows; y++)
       for (let x = 0; x < this._cols; x++)
-        if (false) //Todo: check in the grid if there is a cell
+        if (this._field[y][x] > -1) {
+          switch (this._field[y][x]) {
+            case 0:
+              ctx.fillStyle = '#BBB';
+              break;
+
+            case 1:
+              ctx.fillStyle = '#DDD';
+              break;
+              
+            case 2:
+              ctx.fillStyle = '#FFF';
+              break;
+
+            default:
+              ctx.fillStyle = '#000';
+              break;
+          }
+ 
           ctx.fillRect(11 + x * (2 + this._cellSize), 11 + y * (2 + this._cellSize), this._cellSize, this._cellSize);
+        }
   }
 }
