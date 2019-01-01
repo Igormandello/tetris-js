bd93bc0e322c3accf3e22c8256f92a3d3f88192e
Igor Mandello <igormandello@gmail.com>
2019-01-01 04:22:10 -0200

    Removes the canvas from index.html and construct the classes in main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log(Keys);
+const ge = new GameEngine({ ratio: 9 / 16 }),
+      input = new Input(keyUpdate),
+      res = new Resources('assets', []);
+
+function keyUpdate(event) {
+
+}
+
+res.onload = () => {
+  console.log('loaded');
+}
